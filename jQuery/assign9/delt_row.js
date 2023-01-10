377f488b2c059ef57757f134c8619ce939d7c032
Midhun Madhav P S <77264228+midhunmadhavps@users.noreply.github.com>
2023-01-10 18:34:02 +0530

    Add files via upload

---
+++ jQuery/assign9/delt_row.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#rem1").click(function () {
+        $(this).parent().parent().remove();
+    });
+    $("#rem2").click(function () {
+        $(this).parent().parent().remove();
+    });
+    $("#rem3").click(function () {
+        $(this).parent().parent().remove();
+    });
+    $("#rem4").click(function () {
+        $(this).parent().parent().remove();
+    });
+
+});
