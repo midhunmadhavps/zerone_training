a1f89d0ed06b7724dd89993de3fbd2a395e44dcd
Midhun Madhav P S <77264228+midhunmadhavps@users.noreply.github.com>
2023-01-11 09:17:45 +0530

    Delete select.js

--- jQuery/assig10/select.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("#btn1").click(function () {
-        var Text = $("#text1").val();
-        if (Text.trim() !== "") {
-            $("#select1").append(new Option(Text));
-        }
-    });
-});
-
