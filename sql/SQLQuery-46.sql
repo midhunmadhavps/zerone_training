69990465abaf59d37a2961c2426d51b5ce3f839e
Midhun Madhav P S <77264228+midhunmadhavps@users.noreply.github.com>
2023-01-23 18:02:24 +0530

    Update SQLQuery-46.sql

--- sql/SQLQuery-46.sql
+++ sql/SQLQuery-46.sql
@@ -3,3 +3,13 @@
 create view leastOrders as
 select top 20 OrderID from Orders 
 where OrderDate between DATEADD("m",-6, GETDATE()) and GETDATE()
+
+ass-51
+select Customers.CustomerID,orders.OrderID,Orders.OrderDate
+from Customers left join Orders on Customers.CustomerID = Orders.CustomerID
+where Orders.OrderID is null
+union all
+select min(Customers.CustomerID),min(orders.OrderID),max(Orders.OrderDate)
+from Customers left join Orders on Customers.CustomerID = Orders.CustomerID
+group by  Customers.CustomerID
+having max(Orders.Orderdate)<Dateadd("month",-6,GeTDATE())
